1e80a59071ffdafa3f80f40c49d44c11dc225f02
ShaluthaRajapakshe <shalutha321@gmail.com>
2024-06-04 22:43:35 +0200

    added teleop code

---
+++ joy_teleop/src/joy_controller.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import Pose, PoseStamped
+from geometry_msgs.msg import Twist, TwistStamped
+import transforms3d.quaternions as quat
+import transforms3d.euler as euler
+import numpy as np
+from std_msgs.msg import String
+from sensor_msgs.msg import Joy
+
+class PoseControllerNode(): 
+    def __init__(self):
+        rospy.init_node("joy_controller") 
+
+        # Variables
+        self.freq = 100
+        self.rate = rospy.Rate(self.freq)  
+        self.physical_robot = rospy.get_param("physical_robot")
+        self.frame = "LIO_base_link"
+        self.commandedPose = PoseStamped()
+        self.commandedVel = Twist()
+        self.previous_buttons = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.terminate = False
+
+        # ROS Variables
+        rospy.Subscriber("/joy_driver/commanded_vel", TwistStamped, self.commanded_vel_cb)
+        rospy.Subscriber("/joy", Joy, self.joy_cb)
+
+        self.pose_pub = rospy.Publisher("/commanded_pose", PoseStamped, queue_size=15)
+        self.myp_app_pub = rospy.Publisher("/myp_manager/app_control", String, queue_size=2)
+
+        #Init 
+        self.init_pose()
+        rospy.sleep(1)
+        rospy.loginfo("joy_controller has been started") 
+
+    def joy_cb(self,msg):
+        # Filter buttons input
+        current_buttons = list(msg.buttons)
+        if current_buttons == [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] : changed_buttons = current_buttons
+        else: changed_buttons = [button1 - button2 for (button1, button2) in zip(current_buttons, self.previous_buttons)]
+        self.previous_buttons = current_buttons
+    
+        if changed_buttons[7] != 0:
+            self.terminate = True
+    
+    def commanded_vel_cb(self, msg):
+        self.commandedVel = msg.twist
+    
+    def init_pose(self):
+        self.commandedPose.header.frame_id = self.frame
+
+        # Coordinates in LIO_base_link frame
+        self.commandedPose.pose.position.x=0.20695484883032408
+        self.commandedPose.pose.position.y=0.0005657323485566697
+        self.commandedPose.pose.position.z=1.061217401594137
+
+        self.commandedPose.pose.orientation.x= 0.9982331952531064
+        self.commandedPose.pose.orientation.y= -0.05193699424944531
+        self.commandedPose.pose.orientation.z= -0.02800310197296562
+        self.commandedPose.pose.orientation.w= 0.00699019334487028
+    
+    def run(self):
+        if self.physical_robot: self.myp_app_pub.publish("start")
+        while not rospy.is_shutdown():
+            if self.terminate: 
+                if self.physical_robot: self.myp_app_pub.publish("stop")
+                rospy.sleep(1)
+                return
+            self.update_joy_pose()
+            self.publish_commanded_pose()
+            self.rate.sleep()
+
+    def update_joy_pose(self):
+        self.commandedPose.pose.position.x+=self.commandedVel.linear.x/self.freq
+        self.commandedPose.pose.position.y+=self.commandedVel.linear.y/self.freq
+        self.commandedPose.pose.position.z+=self.commandedVel.linear.z/self.freq
+
+
+        # Quaternion initialization
+        robot_rot = quat.quat2mat([self.commandedPose.pose.orientation.x, self.commandedPose.pose.orientation.y,
+                                self.commandedPose.pose.orientation.z, self.commandedPose.pose.orientation.w])
+        
+        # Mapping fix to obtain same joystick control as drone_panda
+        # Probably due to transforms3d library differences with C++ KDL library
+        ang_vel_x = self.commandedVel.angular.z
+        ang_vel_y = -self.commandedVel.angular.x
+        ang_vel_z = self.commandedVel.angular.y
+
+        # Rotation initialization using Roll-Pitch-Yaw (RPY)
+        motion = euler.euler2mat(ang_vel_x/self.freq, ang_vel_y/self.freq, ang_vel_z/self.freq)
+
+        # Quaternion multiplication and update
+        result_quat = quat.mat2quat(np.dot(motion, robot_rot))
+        self.commandedPose.pose.orientation.x, self.commandedPose.pose.orientation.y, self.commandedPose.pose.orientation.z, self.commandedPose.pose.orientation.w = result_quat
+
+        # Quaternion normalization
+        norm = np.linalg.norm([self.commandedPose.pose.orientation.x, self.commandedPose.pose.orientation.y,
+                            self.commandedPose.pose.orientation.z, self.commandedPose.pose.orientation.w])
+        if norm != 1:
+            self.commandedPose.pose.orientation.x /= norm
+            self.commandedPose.pose.orientation.y /= norm
+            self.commandedPose.pose.orientation.z /= norm
+            self.commandedPose.pose.orientation.w /= norm
+    
+    def publish_commanded_pose(self):
+        self.commandedPose.header.stamp = rospy.Time.now()
+        self.pose_pub.publish(self.commandedPose)
+
+
+
+
+if __name__ == "__main__":
+    my_node = PoseControllerNode()  
+    my_node.run()
---
+++ joy_teleop/src/joy_driver.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import Twist, TwistStamped
+from sensor_msgs.msg import Joy
+from std_msgs.msg import String
+from tf.transformations import *
+import signal
+import sys
+import numpy as np
+
+class JoyDriver(object):
+    def __init__(self):
+        rospy.init_node('joy_driver')
+
+        self._twist_pub = rospy.Publisher("/joy_driver/commanded_vel", TwistStamped, queue_size=1)
+        self._command_pub = rospy.Publisher("/gripper_state", String, queue_size=1)
+        self._joy_sub = rospy.Subscriber('/joy', Joy, self.on_joy)
+
+        self._ax2_init = False
+        self._ax5_init = False      
+        self._a_pressed = False
+
+        rospy.loginfo("joy_driver has been started") 
+      
+
+    def mapping(self,x,low=0.005,high=.25):
+        a = (np.log(high)-np.log(low))/0.9
+        b = np.exp(np.log(low)-.1*a)
+        return np.sign(x)*b*((np.exp(a*np.abs(x))-1))
+        
+    def on_joy(self, msg):
+        ax = np.array(msg.axes)
+        if ax[2] != 0:
+            self._ax2_init = True
+        if not self._ax2_init:
+            ax[2]=1
+        if ax[5] != 0:
+            self._ax5_init = True
+        if not self._ax5_init:
+            ax[5]=1
+        t = Twist()
+        t.linear.x = self.mapping(ax[1])
+        t.linear.y = self.mapping(ax[0])
+        t.linear.z = self.mapping((ax[2]-ax[5])/2)
+        t.angular.x = self.mapping(-ax[3],low=.01,high=1)
+        t.angular.y = self.mapping(ax[4],low=.01,high=1)
+        if msg.buttons[4]:
+            t.angular.z = -np.pi/4
+        if msg.buttons[5]:
+            t.angular.z = np.pi/4
+        new_msg = TwistStamped()
+        new_msg.twist=t
+        self._twist_pub.publish(new_msg)
+
+        if msg.buttons[0] and not self._a_pressed:
+            self._a_pressed = True
+            self._command_pub.publish("toggle_gripper")
+        
+        if not msg.buttons[0] and self._a_pressed:
+            self._a_pressed = False
+    
+    def run(self):
+        rospy.spin()
+
+    def signal_handler(self, signal, frame):
+        sys.exit()
+
+if __name__ == "__main__":
+    mouse_driver = JoyDriver()
+    signal.signal(signal.SIGINT, mouse_driver.signal_handler)
+    mouse_driver.run()
